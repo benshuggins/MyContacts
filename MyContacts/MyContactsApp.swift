43807b1ca3f0eedc423e61d57c48293f7e159cfb
Ben Huggins <benshuggins@gmail.com>
2023-10-10 09:40:11 -0700

    first commit

---
+++ MyContacts/MyContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyContactsApp.swift
+//  MyContacts
+//
+//  Created by Ben Huggins on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MyContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
